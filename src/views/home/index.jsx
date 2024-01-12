30371be9a77245743160910b3609e0253f6297de
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-01-12 07:28:14 +0100

    starting on the navbar

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Navbar from 'components/navbar';
 import Hero from './s1_hero';
 import JoinUs from './s2_joinus';
 
 const Home = () => {
   return (
+    <>
+    <Navbar />
     <div>
       <Hero />
       <JoinUs />
     </div>
+    </>
   )
 }
 
