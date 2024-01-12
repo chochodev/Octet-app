63c719e154916f9ecc71d5fe10721f780b9b4a81
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-01-12 08:58:44 +0100

    starting on the hero section

--- src/views/home/s1_hero/index.jsx
+++ src/views/home/s1_hero/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
-const Home = (props) => {
+const Hero = () => {
   return (
-    <div>
-      <h2>{props.title}</h2>
+    <div className='relative flex w-full'>
+      <div className="flex flex-col">
+        <h1>We are moving around the world with the OCTET project at the forefront to detect malicious code.</h1>
+        <p>Please contribute to the project by submitting malicious codes. And you can earn rewards.</p>
+        <div className="flex">
+          <Link to='joinus' className=''>Join Community</Link>
+          <Link to='whitepaper' className=''>Whitepaper</Link>
+        </div>
+      </div>
+      <img src="./assets/images/hero-image-1.png" alt="HERO" className='' />
     </div>
   )
 }
 
-Home.defaultProps = {
-  title: 'Home',
-  style: ''
-};
-
-export default Home;
+export default Hero
