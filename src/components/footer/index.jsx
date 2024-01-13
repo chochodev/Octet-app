4ff53c8a831c6305b9560ec5ae94c74739175db7
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-01-13 16:29:18 +0100

    setting up octetapp section

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-col md:flex-row gap-y-[2rem] justify-center md:justify-evenly items-center min-h-[10.5rem] w-full p-[6%]'>
+      <div className='flex flex-1 items-center gap-[2%] md:gap-[5%] justify-center w-full'>
+        <Link to='home' className='text-[.75rem] md:text-[1rem] hover:text-secondary active:text-secondary-dim transition-all duration-200 ease-in-out'>Home</Link>
+        <Link to='joinus' className='text-[.75rem] md:text-[1rem] hover:text-secondary active:text-secondary-dim transition-all duration-200 ease-in-out'>Join Us</Link>
+        <Link to='roadmap' className='text-[.75rem] md:text-[1rem] hover:text-secondary active:text-secondary-dim transition-all duration-200 ease-in-out'>Roadmap</Link>
+        <Link to='tokenomic' className='text-[.75rem] md:text-[1rem] hover:text-secondary active:text-secondary-dim transition-all duration-200 ease-in-out'>Tokenomics</Link>
+        <Link to='octapp' className='text-[.75rem] md:text-[1rem] hover:text-secondary active:text-secondary-dim transition-all duration-200 ease-in-out uppercase'>Oct App</Link>
+      </div>
+      <div className='flex gap-[15%] justify-center min-w-[30%]'>
+        <a href='#home'>
+          <img src="./assets/svg/telegram.svg" alt="TELEGRAM" className='w-[3rem] h-[3rem] object-contain' />
+        </a>
+        <a href='#home'>
+          <img src="./assets/svg/discord.svg" alt="DISCORD" className='w-[3rem] h-[3rem] object-contain' />
+        </a>
+        <a href='#home'>
+          <img src="./assets/svg/x.svg" alt="X" className='w-[3rem] h-[3rem] object-contain' />
+        </a>
+      </div>
+    </div>
   )
 }
 
