9b27e385b451b701df9f9b88dcb8c0bebf980096
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-01-19 10:50:18 +0100

    finishing the footer

--- src/views/home/s1_hero/index.jsx
+++ src/views/home/s1_hero/index.jsx
@@ -14,7 +14,6 @@ const Hero = () => {
         <div className="flex w-full xl:max-w-[46rem]">
           <a 
             href='https://t.me/octetproject' 
-            rel="noopener noreferrer" 
             target="_blank" 
             className={join_community}
           >
